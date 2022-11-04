4fed40a5b00ee89de33c1127297d76b577011ef0
Kiriesha <49344266+Kiriesha@users.noreply.github.com>
2022-11-04 17:04:46 +0300

    add attach

--- src/test/java/test/demoqa/TestBase.java
+++ src/test/java/test/demoqa/TestBase.java
@@ -1,6 +1,9 @@
 package test.demoqa;
 
+import com.beust.ah.A;
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -18,7 +21,16 @@ public class TestBase {
         //Добавляем URL, чтоб не прописывать каждый раз
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //Configuration.browser = "chrome";
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
     }
 }
 
