0830f9e285eedf27c9dbcabf48b8f891c8d45532
Kiriesha <49344266+Kiriesha@users.noreply.github.com>
2022-11-04 17:42:54 +0300

    add picture

--- src/test/java/test/demoqa/TestBase.java
+++ src/test/java/test/demoqa/TestBase.java
@@ -2,9 +2,13 @@ package test.demoqa;
 
 import com.beust.ah.A;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -17,11 +21,19 @@ public class TestBase {
             year = "1998";
 
     @BeforeAll
-    static void configure(){
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         //Добавляем URL, чтоб не прописывать каждый раз
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub";
         //Configuration.browser = "chrome";
     }
 
